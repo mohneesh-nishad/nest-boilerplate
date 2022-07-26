fe6ddc893047488e9988b33a5c10787730266c5f
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-26 15:20:23 +0530

    [FIX] server exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -13,53 +13,63 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 // import { contentParser } from 'fastify-multer';
 
 async function bootstrap() {
-  const server = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: false }));
-  // server.register(contentParser);
+  // try {
+    const server = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: false }));
+    // server.register(contentParser);
 
-  // Validation
-  server.useGlobalPipes(new ValidationPipe());
+    // Validation
+    server.useGlobalPipes(new ValidationPipe());
 
+    
+    // Prisma Client Exception Filter for unhandled exceptions
+    // const { httpAdapter } = server.get(HttpAdapterHost);
+    // server.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  // Prisma Client Exception Filter for unhandled exceptions
-  // const { httpAdapter } = server.get(HttpAdapterHost);
-  // server.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+    const configService = server.get(ConfigService);
+    const nestConfig = configService.get<NestConfig>('nest');
+    const corsConfig = configService.get<CorsConfig>('cors');
+    const swaggerConfig = configService.get<SwaggerConfig>('swagger');
 
-  const configService = server.get(ConfigService);
-  const nestConfig = configService.get<NestConfig>('nest');
-  const corsConfig = configService.get<CorsConfig>('cors');
-  const swaggerConfig = configService.get<SwaggerConfig>('swagger');
+    // Swagger Api
+    if (swaggerConfig.enabled) {
+      const options = new DocumentBuilder()
+        .setTitle(swaggerConfig.title || 'Nestjs')
+        .setDescription(swaggerConfig.description || 'The nestjs API description')
+        .setVersion(swaggerConfig.version || '1.0')
+        .build();
+      const document = SwaggerModule.createDocument(server, options);
 
-  // Swagger Api
-  if (swaggerConfig.enabled) {
-    const options = new DocumentBuilder()
-      .setTitle(swaggerConfig.title || 'Nestjs')
-      .setDescription(swaggerConfig.description || 'The nestjs API description')
-      .setVersion(swaggerConfig.version || '1.0')
-      .build();
-    const document = SwaggerModule.createDocument(server, options);
-
-    SwaggerModule.setup(swaggerConfig.path || 'api', server, document);
-  }
-
-  // Cors
-  // if (corsConfig.enabled) {
-  //   // server.enableCors();
-  // }
-  server.enableShutdownHooks()
-
-  const PORT = parseInt(process.env.PORT)
-  //* 0.0.0.0 fro docker network compatibility.
-  const SERVER_ADDRESS = '0.0.0.0'
-
-  server.listen(PORT, SERVER_ADDRESS, (err, address) => {
-    if (err) {
-      console.log('error from listener')
-      console.log(err);
-      process.exit(1);
-    } else {
-      console.log(`Application is running on: ${address}`);
+      SwaggerModule.setup(swaggerConfig.path || 'api', server, document);
     }
-  });
+
+    // Cors
+    // if (corsConfig.enabled) {
+    //   // server.enableCors();
+    // }
+    server.enableShutdownHooks()
+
+    const PORT = parseInt(process.env.PORT)
+    //* 0.0.0.0 fro docker network compatibility.
+    const SERVER_ADDRESS = '0.0.0.0'
+
+    await server.listen(PORT, SERVER_ADDRESS, (err, address) => {
+      if (err) {
+        console.log('error from listener')
+        console.log(err);
+        process.exit(1);
+      } else {
+        console.log(`Application is running on: ${address}`);
+      }
+    })
+  // }
+  // catch (error) {
+  //   Logger.error(`❌  Error starting server internal, ${error}`, '', 'Bootstrap', false)
+  //   process.exit()
+  // }
 }
 
-bootstrap();
+bootstrap().catch(e => {
+  Logger.error(`❌  Error starting server external, ${e}`, '', 'Bootstrap', false)
+  process.exit(1)
+  throw e
+});
