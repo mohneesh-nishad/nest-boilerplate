1473cd86477cdb08ea88f14195a3b710117837bc
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-27 15:42:07 +0530

    [FIX] model association with User, deletedAt field usage

--- src/user-profile/entities/user-profile.entity.ts
+++ src/user-profile/entities/user-profile.entity.ts
@@ -1,43 +1,49 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { BelongsTo, Column, ForeignKey, HasOne, Model, Table, Unique } from "sequelize-typescript";
-import { DataTypes } from "sequelize";
+import { Field, HideField, ObjectType } from "@nestjs/graphql";
+import { BelongsTo, Column, DataType, DeletedAt, ForeignKey, HasOne, Model, Table, Unique } from "sequelize-typescript";
 import { Gender } from "src/common/enums/user-gender.enum";
 import { User } from "../../users/entities/users.entity";
 
-@Table({ tableName: 'userProfiles' })
-@ObjectType('userProfile')
+@Table({ tableName: 'userProfiles', timestamps: true })
+@ObjectType('UserProfile')
 export class UserProfile extends Model<UserProfile> {
+
     @Field()
-    @Unique
-    @Column({ type: DataTypes.INTEGER, allowNull: false })
+    @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true })
+    id: number;
+
+    @Field()
+    @Unique({ name: 'userId', msg: 'It must be unique' })
+    @Column({ type: DataType.INTEGER, allowNull: false })
     @ForeignKey(() => User)
     // @BelongsTo(() => User, 'userId')
     userId: number;
 
     @Field(() => Gender, { nullable: true })
-    @Column({ type: DataTypes.STRING, allowNull: true, defaultValue: null })
+    @Column({ type: DataType.STRING, allowNull: true, defaultValue: null })
     gender: Gender;
 
     @Field({ nullable: true })
-    @Column({ type: DataTypes.TEXT, allowNull: true, defaultValue: null })
+    @Column({ type: DataType.TEXT, allowNull: true, defaultValue: null })
     job: string;
 
     @Field({ nullable: true })
-    @Column({ type: DataTypes.STRING, allowNull: true, defaultValue: null })
+    @Column({ type: DataType.STRING, allowNull: true, defaultValue: null })
     country: string;
 
     @Field({ nullable: true })
-    @Column({ type: DataTypes.STRING, allowNull: true, defaultValue: null })
+    @Column({ type: DataType.STRING, allowNull: true, defaultValue: null })
     website: string;
 
     @Field({ nullable: true })
-    @Column({ type: DataTypes.STRING, allowNull: true, defaultValue: null })
+    @Column({ type: DataType.STRING, allowNull: true, defaultValue: null })
     bio: string;
 
     @Field({ nullable: true })
-    @Column({ type: DataTypes.BOOLEAN, defaultValue: false })
-    isDeleted: Boolean
+    @DeletedAt
+    isDeleted: Date
 
-    @HasOne(() => User)
+    @HideField()
+    // @ForeignKey(() => User)
+    @BelongsTo(() => User)
     user: User
 }
