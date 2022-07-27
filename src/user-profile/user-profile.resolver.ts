4befb901193c4ee8a695680d0e9f6eb1e99c834d
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-27 15:41:33 +0530

    [FIX] query and mutation

--- src/user-profile/user-profile.resolver.ts
+++ src/user-profile/user-profile.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
 import { UserEntity } from 'src/common/decorators/user.decorator';
+import { ProfileResponse, UserResponse } from 'src/common/responses';
 import { User } from 'src/users/entities';
 import { UpdateProfileInput } from './dto/update-profile.input';
 import { UserProfileService } from './user-profile.service';
@@ -12,12 +13,16 @@ import { UserProfileService } from './user-profile.service';
 export class UserProfileResolver {
   constructor(private readonly userProfileService: UserProfileService) { }
 
-  @Mutation(() => User)
-  async updateUserProfile(@Args('data') args: UpdateProfileInput, @UserEntity() user: User) {
+  @Query(() => ProfileResponse, { name: 'getMyProfile' })
+  async getUserProfile(@UserEntity() user: User) {
+    return this.userProfileService.getProfileByUserId(user.id)
+  }
+
+
+  @Mutation(() => UserResponse, { name: 'updateUserProfile' })
+  async updateUserProfile(@Args('data') args: UpdateProfileInput, @UserEntity() user: User): Promise<any> {
     const { gender, job, country, website, bio } = args
-    console.log(args)
-    console.log(user)
-    // return {}
-    return this.userProfileService.createProfile(user.id, args)
+    return this.userProfileService.updateProfile(user, args)
+
   }
 }
--- src/user-profile/user-profile.service.ts
+++ src/user-profile/user-profile.service.ts
@@ -1,5 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { where } from 'sequelize/types';
+import { Inject, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { USER_PROFILE_REPOSITORY } from 'src/core/constants';
 import { User } from 'src/users/entities';
 import { UpdateProfileInput } from './dto/update-profile.input';
@@ -9,13 +8,36 @@ import { UserProfile } from './entities';
 export class UserProfileService {
     constructor(@Inject(USER_PROFILE_REPOSITORY) private readonly profileRepo: typeof UserProfile) { }
 
-    async createProfile(userId: number, payload: UpdateProfileInput) {
-        const profile = await this.profileRepo.create({ userId, ...payload }, {
-            include: {
-                model: User, where: { id: userId }
+    async createProfile(user: User, payload: UpdateProfileInput) {
+        const profile = await this.profileRepo.create({ userId: user.id, ...payload })
+        // console.log(profile.toJSON())
+        const result = { ...user.toJSON(), profile }
+        return { payload: result, status: 200, msg: 'created profile' }
+        // return profile.toJSON()
+    }
+
+
+    async getProfileByUserId(userId: number) {
+        const profile = await this.profileRepo.findOne({ where: { userId: userId } })
+        if (!profile) throw new NotFoundException('User Profile Not Found')
+        // console.log(profile.toJSON())
+        return { payload: profile, status: 200, msg: 'User profile fetched' }
+    }
+
+
+    async updateProfile(user: User, args: UpdateProfileInput) {
+        try {
+            const getProfile = await this.profileRepo.findOne({ where: { userId: user.id } })
+            if (getProfile) {
+                const [success, rows] = await this.profileRepo.update(args, { where: { userId: user.id }, returning: true })
+                if (!success) throw new InternalServerErrorException
+                const result = { ...user.toJSON(), profile: rows[0].toJSON() }
+                return { payload: result, status: 200, msg: 'User profile updated' }
+            } else {
+                return this.createProfile(user, args)
             }
-        })
-        console.log(profile)
-        return profile.toJSON()
+        } catch (error) {
+            throw new Error(error)
+        }
     }
 }
