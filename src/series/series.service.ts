aefaae0888d8be8a923bbd62114e59c9a66f0f95
mohneesh-nishad <mohneesh@noborderz.com>
2022-07-29 13:20:12 +0530

    [ADD] create method

--- src/series/series.service.ts
+++ src/series/series.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { SERIES_REPOSITORY } from 'src/core/constants';
+import { SeriesCreateDto } from './dto/create-series.input';
+import { Series } from './entities';
 
 @Injectable()
-export class SeriesService {}
+export class SeriesService {
+    constructor(@Inject(SERIES_REPOSITORY) private readonly repo: typeof Series) { }
+
+    async create(input: SeriesCreateDto) {
+        return this.repo.create({ ...input })
+    }
+}
